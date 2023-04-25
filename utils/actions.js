053675b80d99d5f3e6908e5f6d97e0c6da320b8b
Sakinah Freeman <freemansakinah@gmail.com>
2023-04-24 21:24:15 -0400

    creates actions for note schema

---
+++ utils/actions.js
@@ -0,0 +1,16 @@
+import { Note } from "./models/noteSchema";
+
+// get all notes
+export const getNotes = async () => await Note.find({})
+
+// get a single note
+export const getNote = async (id) => await Note.findById(id)
+
+// create a new note
+export const createNote = async (newNote) => await Note.create(newNote)
+
+// update a note
+export const updateNote = async (id, updatedNote) => await Note.findByIdAndUpdate(id, updatedNote, {new: true})
+
+// delete a note
+export const deleteNote = async (id) => await Note.findByIdAndDelete(id)
