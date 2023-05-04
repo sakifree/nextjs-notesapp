3b48f1ca46f8f84f6f8c06c2488899694a9ba1cb
Sakinah Freeman <freemansakinah@gmail.com>
2023-05-04 19:45:36 -0400

    displays title and text on individual page

--- pages/notebook/[id].js
+++ pages/notebook/[id].js
@@ -1,11 +1,17 @@
 import { getNote } from "@/utils/actions"
+import { useRouter } from "next/router"
 import { useState } from "react"
+import QuillNoSSRWrapper from "@/components/Quill/QuillWrapper"
 
 const Show = ({note}) => {
     console.log(note)
 
+    const router = useRouter()
+
     return <>
         <h1>Show Page</h1>
+        <h1>{note.title}</h1>
+        <QuillNoSSRWrapper value={note.text} readOnly={true} theme="bubble" />
     </>
 }
 
