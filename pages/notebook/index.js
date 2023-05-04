912b919cc02f40d40df4bbc414c3b4c3c1414d9b
Sakinah Freeman <freemansakinah@gmail.com>
2023-05-04 18:54:07 -0400

    grabs information from db

--- pages/notebook/index.js
+++ pages/notebook/index.js
@@ -1,3 +1,6 @@
+import { getNotes } from "@/utils/actions"
+import Link from "next/link"
+
 const Index = (props) => {
     return <>
         <h1>Index Page</h1>
@@ -5,3 +8,14 @@ const Index = (props) => {
 }
 
 export default Index
+
+export async function getServerSideProps(context){
+    const notes = JSON.parse(JSON.stringify(await getNotes()))
+    console.log(notes)
+
+    return {
+        props: {
+            notes
+        }
+    }
+}
