8106dc4bd8f51aa2040275ed26782924b9ebbc4d
Sakinah Freeman <freemansakinah@gmail.com>
2023-05-04 19:21:07 -0400

    notes show on index page

--- pages/notebook/index.js
+++ pages/notebook/index.js
@@ -1,17 +1,28 @@
 import { getNotes } from "@/utils/actions"
 import Link from "next/link"
 
-const Index = (props) => {
-    return <>
-        <h1>Index Page</h1>
-    </>
+const Index = ({notes}) => {
+    return (
+        <>
+            <h1>Index Page</h1>
+            <div>
+                {notes.map((note) => (
+                    <div key={note._id}>
+                        <Link href={`/notebook/${encodeURIComponent(note._id)}`}>
+                            <h2>{note.title}</h2>
+                        </Link>
+                    </div>
+                ))}
+            </div>
+        </>
+    )
 }
 
 export default Index
 
 export async function getServerSideProps(context){
     const notes = JSON.parse(JSON.stringify(await getNotes()))
-    console.log(notes)
+    //console.log(notes)
 
     return {
         props: {
